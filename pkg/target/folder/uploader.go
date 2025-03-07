1ea33b69a8658a94b8385ba3e78967105aef79e9
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2025-03-07 23:42:54 +0530

    add parallel upload for folder
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- pkg/target/folder/uploader.go
+++ pkg/target/folder/uploader.go
@@ -19,6 +19,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/google/uuid"
 	"github.com/interlynk-io/sbommv/pkg/iterator"
@@ -33,8 +34,7 @@ type SBOMUploader interface {
 
 var uploaderFactory = map[types.UploadMode]SBOMUploader{
 	types.UploadSequential: &SequentialUploader{},
-	// types.UploadParallel:   &ParallelUploader{},
-	// Add parallel uploader later
+	types.UploadParallel:   &ParallelUploader{}, // Add parallel uploader later
 }
 
 type SequentialUploader struct{}
@@ -74,4 +74,70 @@ func (u *SequentialUploader) Upload(ctx *tcontext.TransferMetadata, config *Fold
 	return nil
 }
 
-type ParallelUploader struct{}
+type ParallelUploader struct {
+	MaxWorkers int
+}
+
+func (u *ParallelUploader) Upload(ctx *tcontext.TransferMetadata, config *FolderConfig, iter iterator.SBOMIterator) error {
+	logger.LogDebug(ctx.Context, "Writing SBOMs in parallel", "folder", config.FolderPath)
+
+	u.MaxWorkers = 3
+	taskChan := make(chan *iterator.SBOM)
+	var wg sync.WaitGroup
+	errChan := make(chan error, u.MaxWorkers)
+
+	for i := 0; i < u.MaxWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for sbom := range taskChan {
+				if err := u.processSBOM(ctx, config, sbom); err != nil {
+					errChan <- err
+				}
+			}
+		}()
+	}
+	for {
+		sbom, err := iter.Next(*ctx)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			logger.LogError(ctx.Context, err, "Error retrieving SBOM from iterator")
+			continue
+		}
+		taskChan <- sbom
+	}
+	close(taskChan)
+
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		logger.LogError(ctx.Context, err, "Error during parallel upload")
+	}
+
+	logger.LogDebug(ctx.Context, "Successfully Writen SBOMs in parallel", "folder", config.FolderPath)
+
+	return nil
+}
+
+func (u *ParallelUploader) processSBOM(ctx *tcontext.TransferMetadata, config *FolderConfig, sbom *iterator.SBOM) error {
+	namespace := filepath.Base(sbom.Namespace)
+	if namespace == "" {
+		namespace = fmt.Sprintf("sbom_%s.json", uuid.New().String())
+	}
+	outputDir := filepath.Join(config.FolderPath, namespace)
+	if err := os.MkdirAll(outputDir, 0o755); err != nil {
+		return fmt.Errorf("failed to create folder %s: %w", outputDir, err)
+	}
+	outputFile := filepath.Join(outputDir, sbom.Path)
+	if sbom.Path == "" {
+		outputFile = filepath.Join(outputDir, fmt.Sprintf("%s.sbom.json", uuid.New().String()))
+	}
+	if err := os.WriteFile(outputFile, sbom.Data, 0o644); err != nil {
+		return fmt.Errorf("failed to write SBOM file %s: %w", outputFile, err)
+	}
+	logger.LogDebug(ctx.Context, "Successfully written SBOM", "path", outputFile)
+	return nil
+}
